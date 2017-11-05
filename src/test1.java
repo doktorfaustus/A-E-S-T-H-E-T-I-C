4fca8c5465eee58c8af38aff509a98e593b6ea0f
jakob <jakob@antergosvm>
2017-11-05 14:51:30 +0100

    soso

---
+++ src/test1.java
@@ -0,0 +1,19 @@
+
+public class test1 {
+	public static int wurzel(int zahl) {
+		int golem = 1;
+		while(golem<=zahl) {
+			if(golem*golem == zahl){
+				return golem;
+			}else{
+				golem = golem + 1 ;
+			}
+		}
+		System.out.println("Zu dieser Zahl gibt es keine natürliche Wurzel!");
+		return 0;
+	}
+	public static void main(String[] args) {
+		System.out.println(wurzel(24));
+	}
+}
+
